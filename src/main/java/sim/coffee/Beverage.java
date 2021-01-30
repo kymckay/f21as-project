f9adc3a4aa16fb779f5816cde67ed759e8dae1cf
linarietuma <linarietuma@192.168.31.119>
2021-01-30 11:53:14 +0200

    Updated Beverage and MenuItem classes.

--- src/main/java/sim/coffee/Beverage.java
+++ src/main/java/sim/coffee/Beverage.java
@@ -1,5 +1,79 @@
 package sim.coffee;
+import java.math.BigDecimal;
 
-public class Beverage {
-    
+public class Beverage extends MenuItem {
+	
+	//instance variables
+	public  enum    Size {S, M, L};
+	private Size    size;
+	private boolean hot;
+	public  enum    Milk {NONE, REGULAR, OAT, SOY};
+	private Milk    milk;
+
+	//constructor
+	public Beverage(String size, String hot, String milk, 
+					String id, BigDecimal price, String description) {
+		
+		super(id, price, description);
+	
+		switch(size.toUpperCase()) { //input recognized regardless of case
+			case "S":
+				this.size = Size.S;
+			break;
+			case "M":
+				this.size = Size.M;
+			break;
+			case "L":
+				this.size = Size.L;
+			break;
+		}
+		
+		switch(hot.toUpperCase()) {
+			case "HOT":
+				this.hot  = true;
+			break;
+			
+			case "COLD": 
+				this.hot = false;
+			break;
+		}
+			
+		switch(milk.toUpperCase()) {
+			case "NONE":
+				this.milk = Milk.NONE;
+			break;
+			case "REGULAR":
+				this.milk = Milk.REGULAR;
+			break;
+			case "OAT":
+				this.milk = Milk.OAT;
+			break;
+			case "SOY":
+				this.milk = Milk.SOY;
+			break;
+		}
+	}
+	
+	public Size getSize() {
+		return size;
+	}
+	
+	public boolean isHot() {
+		return hot;
+	}
+	
+	public Milk getMilk() {
+		return milk;
+	}
+	
+	//overrides toStirng() method
+	public String toString() {
+		String string = "";
+		string += super.getID()  + " ";
+		string += super.getDescription() + " ";
+		string += getMilk() + " ";
+		string += getSize() + " £";
+		string += super.getPrice();
+		return string;
+	}
 }
--- src/main/java/sim/coffee/MenuItem.java
+++ src/main/java/sim/coffee/MenuItem.java
@@ -1,5 +1,39 @@
 package sim.coffee;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-public class MenuItem {
+abstract public class MenuItem {
     
+	//instance variables 
+	private String     id;
+	private BigDecimal price;
+	private String     description;
+	
+	//constructor
+	public MenuItem (String id, BigDecimal price, String description) {
+		
+		this.id          = id;
+		this.price       = price; 
+		this.description = description;
+		
+	}
+	
+	//getter methods
+	public String getID() {
+		return id;
+	}
+	
+	public BigDecimal getPrice() {
+		return price.setScale(2, RoundingMode.DOWN); //rounds the number to two decimal places
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	//overrides toStirng() method
+	//figured it would make sense for each subclass to have its own toString() given the additional info (e.g. size for beverages)
+	public abstract String toString(); 
+	
+ 	
 }
