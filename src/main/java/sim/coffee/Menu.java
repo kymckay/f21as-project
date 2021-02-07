704055e6b69ec21386a20116c1d56ff23dd60860
arif <arif@model-x>
2021-02-07 21:54:36 +0000

    Created readFile()

--- src/main/java/sim/coffee/Menu.java
+++ src/main/java/sim/coffee/Menu.java
@@ -1,5 +1,22 @@
 package sim.coffee;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Menu {
     
+	
+	public void readFile() throws FileNotFoundException
+	{
+		File file = new File("input.txt");
+		Scanner scan = new Scanner(file);
+		
+		while(scan.hasNextLine()) {
+			System.out.println(scan.nextLine());
+		}
+	
+	}	
 }
