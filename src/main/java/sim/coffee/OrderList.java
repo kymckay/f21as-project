1c97cbc7a4b8329f0c52efd72e962d53f67d1f9c
Yeshwanthraj19 <yash.90raj@gmail.com>
2021-02-07 21:35:51 +0000

    Created readfile method

--- src/main/java/sim/coffee/OrderList.java
+++ src/main/java/sim/coffee/OrderList.java
@@ -1,5 +1,28 @@
 package sim.coffee;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class OrderList {
+public void readFile() {
+		
+		File inputFileObject = new File("input.txt"); 
+		Scanner scannerObject;
+		
+		try {
+			String[] lines = new String[20];
+			scannerObject = new Scanner(inputFileObject);
+			for(int i=0;scannerObject.hasNextLine();i++) {
+				lines[i]=scannerObject.nextLine();
+			}
+			//chessPlayerList.readChessPlayerListFile(lines);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+			    
+	}
     
 }
