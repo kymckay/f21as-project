6a8c3ebcec8fbbde31cbe832fbbc2718f35eb9ab
arif <arif@model-x>
2021-02-08 18:54:21 +0000

    Added few lines of code.

--- src/main/java/sim/coffee/Menu.java
+++ src/main/java/sim/coffee/Menu.java
@@ -2,21 +2,51 @@ package sim.coffee;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Set;
+
 
 public class Menu {
+	
+	public static void main(String args[]) 
+	{
+	
+		HashMap<Integer, String> menu = new HashMap<Integer, String>();
+	
+		menu.put(null, null);
+		menu.put(null, null);
+		menu.put(null, null);
+		
+		System.out.println("Keyset:" + menu.keySet());
+	
+		/*	public Set<Integer> keySet() {
+	    return menu.keySet();
+	  	}	*/
+	
+		//	Set<String> keys = hmap.keySet();
+	
+		/*	Iterator<Integer> it = keys.iterator();
+	  	while(it.hasNext()){
+	  	System.out.print(it.next());	*/
+	
+	}
     
+	
 	
 	public void readFile() throws FileNotFoundException
 	{
+		
 		File file = new File("input.txt");
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(file);
 		
-		while(scan.hasNextLine()) {
+		while(scan.hasNextLine()) 
+		{
 			System.out.println(scan.nextLine());
 		}
 	
 	}	
+
 }
