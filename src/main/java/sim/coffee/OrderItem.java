e3368f149a9381cc8ef52230fb16149135dd9485
Jonathan Lee <jl69@hw.ac.uk>
2021-02-13 14:29:02 +0000

    OrderItem

--- src/main/java/sim/coffee/OrderItem.java
+++ src/main/java/sim/coffee/OrderItem.java
@@ -1,5 +1,15 @@
 package sim.coffee;
 
+import java.math.BigDecimal;
+
 public abstract class OrderItem {
+
+    protected MenuItem menuItem;
+
+    public OrderItem(MenuItem m) {
+        menuItem = m;
+    }
+    
+    abstract BigDecimal getPrice();
 
 }
