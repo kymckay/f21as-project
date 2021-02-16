0a58fed3414ddacb563e171c6adfdc2c3ec04ad6
Jonathan Lee <jl69@hw.ac.uk>
2021-02-16 11:54:18 +0000

    Update OrderFood.java
    Update getPrice() method

--- src/main/java/sim/coffee/OrderFood.java
+++ src/main/java/sim/coffee/OrderFood.java
@@ -6,14 +6,11 @@ public class OrderFood extends OrderItem {
 
     public OrderFood(MenuItem m) {
         super(m);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public BigDecimal getPrice() {
-        // TODO generate additional cost feature & getBasePrice()
-        return null;
-        // return menuItem.getBasePrice();
+        return menuItem.getPrice();
     }
     
 }
