0428f845261ee6aa712b5f6e45dbd74e8dace3df
Yeshwanthraj19 <yash.90raj@gmail.com>
2021-02-19 21:12:52 +0000

    Added Methods to the final draft

--- src/main/java/sim/coffee/OrderList.java
+++ src/main/java/sim/coffee/OrderList.java
@@ -10,11 +10,12 @@ import java.util.Scanner;
 
 public class OrderList {
 
-	LinkedList<String> OrderList;		// LinkedList has been declared
+	LinkedList<Order> OrderList;	
+	// LinkedList has been declared
 	
 	public OrderList()		// Constructor for orderLisr=t is created
 	{
-		OrderList= new LinkedList<String>();
+		OrderList= new LinkedList<Order>();
 	}	
 	
 	public void readFile(String fileName) {  /* 1. Is process line method same as that in menu class
@@ -24,11 +25,11 @@ public class OrderList {
 		Scanner scannerObject;
 		
 		try {
-			String[] lines = new String[20];
+			
 			scannerObject = new Scanner(inputFileObject);
 			for(int i=0;scannerObject.hasNextLine();i++)
 			{
-				lines[i]=scannerObject.nextLine();
+				processLine(scannerObject.nextLine());
 			}
 			
 		} catch (FileNotFoundException e) {
@@ -38,7 +39,56 @@ public class OrderList {
     
 		public void processLine(String Line)
 		{
-			
+			try
+			{
+				if (Line.split(",").length>=2)
+				{
+					String[] words = Line.split(",");
+					
+					if (words[3].matches("^B"))
+					{
+						OrderBeverage orderBeverage = new OrderBeverage(word[3],word[4],word[5],new MenuItem(word[2],
+															MenuTableModel.getValueAt(word[2],2)),
+															MenuTableModel.getValueAt(word[2],1))
+															)); 
+						OrderList.add(new Order(word[0],word[1],orderBeverage,MenuTableModel.getValueAt(word[2],2))); // use menutable model object and get the price
+						
+					}
+					
+					
+					if (words[4].matches("^F"))
+					{
+
+						OrderFood orderFood = new OrderFood(,new MenuItem(word[2],
+															MenuTableModel.getValueAt(word[2],2)),
+															MenuTableModel.getValueAt(word[2],1))
+															)); 
+						
+						OrderList.add(new Order(word[0],word[1],orderFood,MenuTableModel.getValueAt(word[2],2))); // use menutable model object and get the price
+						
+					}
+
+					
+					if (words[5].matches("^M"))
+					{
+						
+						OrderMerchandise orderMerchandise = new OrderMerchandise(word[3],word[4],new MenuItem(word[2],
+								MenuTableModel.getValueAt(word[2],2)),
+								MenuTableModel.getValueAt(word[2],1))
+								)); 
+
+						OrderList.add(new Order(word[0],word[1],orderFood,MenuTableModel.getValueAt(word[2],2))); // use menutable model object and get the price
+
+					}
+					
+						
+				}
+			}
+		    catch(NumberFormatException e)
+			{
+				System.out.println("Invalid Line");
+				continue;
+			}
 		}
 		
 		public void writeFile(String variable) // Method to write to output file 
@@ -57,7 +107,7 @@ public class OrderList {
 		
 		public boolean add(Order o)   // Method to add values to the LinkedList
 		{
-			return OrderList.add(null);
+			return OrderList.add(o);
 			
 		}
 		
@@ -70,7 +120,18 @@ public class OrderList {
 		
 		public BigDecimal getTotalIncome() // Will this be implemented in GUI ?
 		{
-			return null;
+			BigDecimal sum = 0;
 			
+			for(int i=0;i<OrderList.size();i++)
+			{
+				sum += Order.getPricePaid(); // sum +?d
+			}
+			return sum;
+			
+		}
+		
+		public int size()
+		{
+			return OrderList.size();
 		}
 }
