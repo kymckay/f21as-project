66de9bea97f1d968e16fbaea9cb5bdbdeda7b05b
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-02-21 15:59:17 +0000

    Simplify menu line processing
    I thought this might be the source of some MenuTableModel tests failing,
    but that doesn't look to be the case unfortunately. In any case the code
    should be simpler and a bit more readable now.

--- src/main/java/sim/coffee/Menu.java
+++ src/main/java/sim/coffee/Menu.java
@@ -3,6 +3,7 @@ package sim.coffee;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Set;
@@ -35,91 +36,64 @@ public class Menu {
 	}
 
 	private void processLine (String line) {
+		// Splitting with regex trims excess whitespace near commas
+		String details [] = line.split("\\s*,\\s*");
 
+		String id            = details[0];
+		String description   = details[1];
+		BigDecimal basePrice = new BigDecimal(details[2]);
 
-		try {
-			// Splitting with regex trims excess whitespace near commas
-			String details [] = line.split("\\s*,\\s*");
+		MenuItem newItem;
 
-			String id            = details[0];
-			String description   = details[1];
-			BigDecimal basePrice = new BigDecimal(details[2]);
+		// First character of item ID diferentiates the types
+		switch (id.substring(0,1)) {
+			case "B":
+				boolean isHot = Boolean.parseBoolean(details[3]);
 
-			if (details[0].matches("^B")) { //identify which MenuItem subclass the input line belongs to based on the id
+				Size[] sizes =
+					Arrays.stream(details[4].split("\\s*|\\s*"))
+						.map(String::toUpperCase)
+						.map(Size::valueOf)
+						.toArray(Size[]::new);
 
-				boolean isHot = Boolean.parseBoolean(details[3]); //need to add boolean isHot values to the sample data
+				Milk[] milks =
+					Arrays.stream(details[5].split("\\s*|\\s*"))
+						.map(String::toUpperCase)
+						.map(Milk::valueOf)
+						.toArray(Milk[]::new);
 
-				String[] size    = details[4].split("|"); // a local array list with object from the input line separated by |
-				Size[] sizeEnums = new Size[size.length - 1];
-				for(int i = 0; i < size.length; i++) { //iterate through all the size array's elements and add them to sizeEnums array
-					sizeEnums[i] = Size.valueOf(size[i].toUpperCase());
+				newItem = new Beverage(sizes, isHot, milks, id, basePrice, description);
 
+				break;
+			case "F":
+				DietaryClass[] dietaryClasses =
+					Arrays.stream(details[3].split("\\s*|\\s*"))
+						.map(String::toUpperCase)
+						.map(DietaryClass::valueOf)
+						.toArray(DietaryClass[]::new);
 
-				}
+				newItem = new Food(dietaryClasses, id, basePrice, description);
+				break;
+			case "M":
+				Label[] labels =
+					Arrays.stream(details[3].split("\\s*|\\s*"))
+						.map(String::toUpperCase)
+						.map(Label::valueOf)
+						.toArray(Label[]::new);
 
-				String[] milk      = details[5].split("|");
-				Milk[]   milkEnums = new Milk[milk.length - 1];
-				for(int i = 0; i < milk.length; i++) {
-					milkEnums[i] = Milk.valueOf(milk[i].toUpperCase());
-				}
+				Colour[] colours =
+					Arrays.stream(details[4].split("\\s*|\\s*"))
+						.map(String::toUpperCase)
+						.map(Colour::valueOf)
+						.toArray(Colour[]::new);
 
-				Beverage beverage = new Beverage(sizeEnums, isHot, milkEnums, id, basePrice, description);
-				this.add(id, beverage);
-
-
-			} else if (details[0].matches("^F")) {
-				String[] dietaryClass = details[3].split("|");
-				DietaryClass[] dietaryEnums = new DietaryClass[dietaryClass.length - 1];
-				for(int i = 0; i < dietaryClass.length; i++) {
-					dietaryEnums[i] = DietaryClass.valueOf(dietaryClass[i].replace("\\s","").toUpperCase()); //remove whitespace in the input
-				}
-
-				Food food = new Food(dietaryEnums, id, basePrice, description);
-				this.add(id, food);
-
-
-			} else if (details[0].matches("^M")) {
-				String[] label = details[3].split("|");
-				Label[] labelEnums = new Label[label.length - 1];
-				for(int i = 0; i < label.length; i++) {
-					labelEnums[i] = Label.valueOf(label[i].toUpperCase());
-				}
-
-				String[] colour = details[4].split("|");
-				Colour[] colourEnums = new Colour[colour.length - 1];
-				for(int i = 0; i < colour.length; i++) {
-					colourEnums[i] = Colour.valueOf(colour[i].toUpperCase());
-				}
-
-				Merchandise merch = new Merchandise(labelEnums, colourEnums, id, basePrice, description);
-				this.add(id, merch);
-
-			}
-
+				newItem = new Merchandise(labels, colours, id, basePrice, description);
+				break;
+			default:
+				throw new IllegalArgumentException("Line contains invalid Item ID");
 		}
 
-		catch (NumberFormatException nfe) {
-			String error = "Number formatting error in line  '" + line;
-			System.out.println(error);
-			System.exit(0);
-		}
-
-		catch (ArrayIndexOutOfBoundsException e) {
-			String error = "Array Index Out Of Bounds error: " + e.getMessage();
-			System.out.println(error);
-			System.exit(1);
-		}
-
-		catch (NullPointerException npe)  {
-			System.out.println(npe.getMessage());
-			System.exit(2);
-		}
-
-		catch (IllegalIDException iie) {
-			System.out.println(iie.getMessage());
-			System.exit(3);
-		}
-
+		this.add(id, newItem);
 	}
 
 
