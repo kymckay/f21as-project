31926b456ee544cc9a2856a7b7b25fc1d5f431b2
Jonathan Lee <jl69@hw.ac.uk>
2021-02-22 16:19:59 +0000

    Created method orderCounter

--- src/main/java/sim/coffee/OrderBasket.java
+++ src/main/java/sim/coffee/OrderBasket.java
@@ -1,5 +1,22 @@
 package sim.coffee;
 
-public class OrderBasket {
-    
-}
+import java.util.HashMap;
+
+public class OrderBasket extends OrderList{
+
+    private static Menu menu;
+    private static OrderList order;
+
+    static HashMap<Menu, Integer> count = new HashMap<>();
+
+    OrderBasket(Menu m, OrderList o) {
+        menu = m;
+        order = o;
+    }
+
+    public static void orderCount() {
+        for (Menu i : count.keySet()) {
+            
+        }
+    }
+}    
