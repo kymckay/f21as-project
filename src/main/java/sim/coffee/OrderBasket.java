ea1f5c26b067efbc327c459a0ec99fc98a14b77e
Jonathan Lee <jl69@hw.ac.uk>
2021-02-22 22:02:24 +0000

    Update OrderBasket.java

--- src/main/java/sim/coffee/OrderBasket.java
+++ src/main/java/sim/coffee/OrderBasket.java
@@ -8,18 +8,21 @@ public class OrderBasket extends OrderList {
 
     private OrderList orderList;
 
-    private HashMap<MenuItem, Integer> count = new HashMap<>();
+    private HashMap<String, Integer> count = new HashMap<>();
 
     // Instantiate Map to track order count
     OrderBasket(Menu m, OrderList o) {
         // Duplicate keys from Menu and replace
         // new hashmap's keys with the same key
-        int i = -1;
-        while (i < m.menuMap.size()) {
-            i++;
-            MenuItem key = m.getKey(String.valueOf(i));
-            int value = 0;
-            count.put(key, value);
+            // int i = -1;
+            // while (i < m.keySet().size()) {
+            //     i++;
+            //     String key = String.valueOf(m.getKey(String.valueOf(i)));
+            //     int value = 0;
+            //     count.put(key, value);
+            // }
+        for (String order : m.keySet()) {
+            count.put(order, 0);
         }
         orderList = o;
     }
@@ -27,17 +30,10 @@ public class OrderBasket extends OrderList {
     // adds value to the map
     @Override
     public boolean add(Order o) {
-        if (condition) {
-            super.add(o);
-            MenuItem a = o.getItemDetails().getId();
-            // int i = count.get(a);
-            // i++;
-            count.put(a, count.get(a) + 1);
-            a = directaccess //key to access hashmap
-        } else {
-            return false
-        }
-
+        super.add(o);
+        String a = o.getItemDetails().getId();
+        count.put(a, count.get(a) + 1);
+        return true;
     }
 
     // public static void orderCount() {
