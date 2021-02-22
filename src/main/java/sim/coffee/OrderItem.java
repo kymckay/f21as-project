e747e5ab89e32f40c583a2b33cc6d35c078c4174
Jonathan Lee <jl69@hw.ac.uk>
2021-02-22 20:07:42 +0000

    Update OrderItem.java
    Reimplement Constructor to track id parsed from GUI

--- src/main/java/sim/coffee/OrderItem.java
+++ src/main/java/sim/coffee/OrderItem.java
@@ -3,6 +3,17 @@ package sim.coffee;
 import java.math.BigDecimal;
 
 public abstract class OrderItem {
+
+    private String id;
+
+    OrderItem (String id) {
+        id = this.id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
     abstract BigDecimal getPrice();
 
     // For pretty details display in UI
