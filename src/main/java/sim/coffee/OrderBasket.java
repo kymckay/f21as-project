bd646277a547ba6a1ec3eb65ecd77ea8f1122967
Jonathan Lee <jl69@hw.ac.uk>
2021-02-22 20:05:49 +0000

    Update OrderBasket.java
    Instantiate HashMap to track order count
    - Duplicate Menu Keys
    - add duplicated keys into new hashMap

--- src/main/java/sim/coffee/OrderBasket.java
+++ src/main/java/sim/coffee/OrderBasket.java
@@ -1,22 +1,46 @@
 package sim.coffee;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class OrderBasket extends OrderList{
 
-    private static Menu menu;
-    private static OrderList order;
+    private OrderList orderList;
 
-    static HashMap<Menu, Integer> count = new HashMap<>();
+    private HashMap<MenuItem, Integer> count = new HashMap<>();
 
+    // Instantiate Map to track order count
     OrderBasket(Menu m, OrderList o) {
-        menu = m;
-        order = o;
+        // Duplicate keys from Menu and replace
+        // new hashmap's keys with the same key
+        int i = -1;
+        while (i < m.menuMap.size()) {
+            i++;
+            MenuItem key = m.getKey(String.valueOf(i));
+            int value = 0;
+            count.put(key, value);
+        }
+        orderList = o;
     }
 
-    public static void orderCount() {
-        for (Menu i : count.keySet()) {
-            
-        }
-    }
+    // adds value to the map
+    // @Override
+    // public boolean add(Order o) {
+    //     super.add(o);
+    //     a = o.getItemDetails().getId();
+    //     if
+    //     a = directaccess //key to access hashmap
+    //     return false;
+    // }
+
+    // public static void orderCount() {
+    //     for (Menu i : count.keySet()) {
+    //         if (order.equals(menu.getKey(key))) {
+    //         //If 
+    //         } else {
+                
+    //         }
+    //     }
+    // }
+
 }    
