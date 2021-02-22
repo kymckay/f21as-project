49c09e0a4fb0ddc065653e849edac3977855054e
Jonathan Lee <jl69@hw.ac.uk>
2021-02-22 20:09:25 +0000

    Update OrderFood.java
    Added super to constructor

--- src/main/java/sim/coffee/OrderFood.java
+++ src/main/java/sim/coffee/OrderFood.java
@@ -3,6 +3,11 @@ package sim.coffee;
 import java.math.BigDecimal;
 
 public class OrderFood extends OrderItem {
+    
+    OrderFood(String id) {
+        super(id);
+    }
+
     @Override
     public BigDecimal getPrice() {
         return new BigDecimal("0");
