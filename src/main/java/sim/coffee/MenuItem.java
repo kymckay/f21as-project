e55810eb00090668ae60e5789f0a0bc6b05fa712
Jonathan Lee <jl69@hw.ac.uk>
2021-02-23 12:25:16 +0000

    Update MenuItem.java
    Added orderCount into constructor

--- src/main/java/sim/coffee/MenuItem.java
+++ src/main/java/sim/coffee/MenuItem.java
@@ -8,9 +8,10 @@ abstract class MenuItem {
 	private String     id;
 	private BigDecimal price;
 	private String     description;
+	private int		   orderCount;
 
 	//constructor
-	MenuItem (String categoryID, String id, BigDecimal price, String description) throws IllegalIDException {
+	MenuItem (String categoryID, String id, BigDecimal price, String description, int count) throws IllegalIDException {
     	boolean idMatches 		= id.matches("^[BFM]\\d{3}"); // ID must match the following regex pattern
     	boolean categoryMatches = id.startsWith(categoryID); // ID must start with the correct category ID, defined in each subclass
 
@@ -21,6 +22,7 @@ abstract class MenuItem {
 		this.id = id;
 		this.price = price;
 		this.description = description;
+		orderCount = count;
 	}
 
 	//getter methods
@@ -34,6 +36,10 @@ abstract class MenuItem {
 
 	public String getDescription() {
 		return description;
+	}
+
+	public int getOrderCount() {
+		return orderCount;
 	}
 
 }
