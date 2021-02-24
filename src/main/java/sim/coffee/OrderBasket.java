a0023731c40ae81f77d0cfbe15af2640c4c80615
jonleesy <65177718+jonleesy@users.noreply.github.com>
2021-02-24 14:54:06 +0000

    Update OrderBasket.java

--- src/main/java/sim/coffee/OrderBasket.java
+++ src/main/java/sim/coffee/OrderBasket.java
@@ -25,22 +25,44 @@ public class OrderBasket extends OrderList {
     }
 
     // Parses through basket list and checks for discount
-    // TODO use setPricePaid
     private void applyDiscount() {
         // Loops through the element - order in orderList
         for (Order o : this.orders) {
+            //local Variables shared ∀ discount cases
             BigDecimal price = o.getFullPrice();
             char id = o.getItemDetails().charAt(0);
+            char bevHot = o.getItemDetails().charAt(2);
+            // Switch case for the 3 differnt discount rules based on the order hour
             switch (o.getTime().getHour()) {
+                // For orders ordered at 8.00 a.m. to 10.59 a.m. (Excluding 11.00 a.m.)
+                // Hence, order hours 8, 9, 10
                 case 8: case 9: case 10:
-                    if (id == 'F' && id == 'B') {
+                    if (id == 'M') {
+                        // In cases that its a merch (no discount)
+                        o.setPricePaid(price);
+                    } else if (bevHot == 'F') {
+                        // In cases that its a beverage but not hot (no discount)
+                        o.setPricePaid(price);
+                        // Other cases (with discount)
+                    } else {
                         price = price.multiply(new BigDecimal(7));
                         price = price.divide(new BigDecimal(100), 3, RoundingMode.CEILING);
                         o.setPricePaid(price);
                     }
                     break;
-            
+                
+                // For every food + drink combo, final price is 4.00
+                case 12: case 13:
+                    // Set Final Price
+                    BigDecimal finalPrice = new BigDecimal(4);
+                    
+
+                    break;
+                case 17: case 18:
+
+                    break;
                 default:
+                    
                     break;
             }
         }
