49b5c8556858c4863eefd858c748c2c798a6ac34
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-02-24 17:38:16 +0000

    Check item tally in basket add test

--- src/test/java/sim/coffee/OrderBasketTest.java
+++ src/test/java/sim/coffee/OrderBasketTest.java
@@ -15,14 +15,16 @@ public class OrderBasketTest {
     static OrderBasket testBasket;
     static Order testOrder;
     static OrderList testList;
+    static Menu testMenu;
 
     // If file isn't found tests fail automatically
     @BeforeClass
     public static void init() throws FileNotFoundException {
+        testMenu = new Menu("data/test/menu.csv");
         testList = new OrderList(new ArrayList<>());
 
         testBasket = new OrderBasket(
-            new Menu("data/test/menu.csv"),
+            testMenu,
             testList,
             new ArrayList<>()
         );
@@ -39,13 +41,20 @@ public class OrderBasketTest {
         );
     }
 
+    /**
+     * Tests that upon adding a new order the basket size increases
+     * Also tests that the menu item order tally is incremented
+     * (tallying may be moved to checkout in future)
+     */
     @Test
     public void itemAdded() {
-        int before = testBasket.size();
+        int sizeBefore = testBasket.size();
+        int tallyBefore = testMenu.getKey("F001").getOrderCount();
 
         testBasket.add(testOrder);
 
-        assertEquals(before + 1, testBasket.size());
+        assertEquals(sizeBefore + 1, testBasket.size());
+        assertEquals(tallyBefore + 1, testMenu.getKey("F001").getOrderCount());
     }
 
     /**
