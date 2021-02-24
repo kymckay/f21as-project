ab7d6551f3060f60ad13ed9dc1b6abee4944e0db
jonleesy <65177718+jonleesy@users.noreply.github.com>
2021-02-24 17:41:34 +0000

    Update OrderBasket.java

--- src/main/java/sim/coffee/OrderBasket.java
+++ src/main/java/sim/coffee/OrderBasket.java
@@ -9,6 +9,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class OrderBasket extends OrderList {
@@ -27,66 +29,92 @@ public class OrderBasket extends OrderList {
 
     // Parses through basket list and checks for discount
     private void applyDiscount() {
+
         // Create counter to keep track no. of each type of items ordered
-        int[] count = {0, 0, 0}; // {'F', 'B', 'M'}
+        int[] count = {0, 0, 0, 0}; // {Sandwich, Pastry, 'B', 'M'}
         for (Order o : this.orders) {
             switch (o.getItemDetails().charAt(0)) {
                 case 'F':
-                    count[0]++;
+                    // Use Regex to track the food item and differentiate
+                    // // between pastry and sandwich
+                    String desc = menu.getKey(o.getItemId()).getDescription();
+                    Pattern pattern = Pattern.compile("sandwich", Pattern.CASE_INSENSITIVE);
+                    Matcher matcher = pattern.matcher(desc);
+                    boolean matchFound = matcher.find();
+                    if (matchFound) {
+                        count[0]++; // If matchFound = true, the sandwich count ++
+                    } else {
+                        count[1]++;
+                    }
                     break;
+
                 case 'B':
-                    count[1]++;
-                    break;
-                default:
                     count[2]++;
+                    break;
+
+                default:
+                    count[3]++;
                     break;
             }
         }
         // Get sum of count array
         int sumCount = IntStream.of(count).sum();
-        // Loops through the element - order in orderList
-        for (Order o : this.orders) {
-            //local Variables shared ∀ discount cases
-            BigDecimal price = o.getFullPrice();
-            char id = o.getItemDetails().charAt(0);
-            char bevHot = o.getItemDetails().charAt(2);
-            // Switch case for the 3 differnt discount rules based on the order hour
-            switch (o.getTime().getHour()) {
-                // For orders ordered at 8.00 a.m. to 10.59 a.m. (Excluding 11.00 a.m.)
-                // Hence, order hours 8, 9, 10
-                // Only applies when count[0] = count[1] and count[0], count[1] > 0
-                case 8: case 9: case 10:
-                    if (sumCount > 0 && count[0]==count[1]) {
-                        if (id == 'M') {
-                            // In cases that its a merch (no discount)
-                            o.setPricePaid(price);
-                        } else if (bevHot == 'F' || bevHot == 'f') {
-                            // In cases that its a beverage but not hot (no discount)
-                            o.setPricePaid(price);
-                            // Other cases (with discount)
+
+        // Order has to have more than 1 item
+        if (sumCount > 0) {
+            // Loops through the element - order in orderList
+            for (Order o : this.orders) {
+
+                // local Variables shared ∀ discount cases
+                BigDecimal price = o.getFullPrice();
+                char id = o.getItemDetails().charAt(0);
+                char bevHot = o.getItemDetails().charAt(2);
+
+                // Switch case for the 3 different discount rules based on the order hour
+                switch (o.getTime().getHour()) {
+
+                    // For orders ordered at 8.00 a.m. to 10.59 a.m. (Excluding 11.00 a.m.)
+                    // Hence, order hours 8, 9, 10
+                    // Only applies when count[0] = count[2], count[1] = count[2]
+                    // // and count[0], count[1], count[2] > 0
+                    case 8: case 9: case 10:
+                        if (count[0] == count[2] && count[1] == count[2]) {
+                            if (bevHot == 'F' || bevHot == 'f') {
+                                // In cases that its a beverage but not hot (no discount)
+                                o.setPricePaid(price);
+                                
+                            } else {
+                                // Other cases (with discount)
+                                price = price.multiply(new BigDecimal(7));
+                                price = price.divide(new BigDecimal(100), 3, RoundingMode.CEILING);
+                                o.setPricePaid(price);
+                            }
+                            break;
+
                         } else {
-                            price = price.multiply(new BigDecimal(7));
-                            price = price.divide(new BigDecimal(100), 3, RoundingMode.CEILING);
                             o.setPricePaid(price);
                         }
-                        break;
-                    }
-                
-                // For every food + drink combo, final price is 4.00
-                // For orders made between 12.00 p.m. to 13.59 p.m.
-                case 12: case 13:
-                    // Set Final Price
-                    BigDecimal finalPrice = new BigDecimal(4);
                     
+                    // For every food + drink combo, final price is 4.00
+                    // For orders made between 12.00 p.m. to 13.59 p.m.
+                    case 12: case 13:
+                        // Sets Meal Deal Price
+                        BigDecimal finalPrice = new BigDecimal(4);
+                        if (condition) {
+                            
+                        } else {
+                            
+                        }
 
-                    break;
-                case 17: case 18:
+                        break;
+                    case 17: case 18:
 
-                    break;
+                        break;
 
-                default:
-                    o.setPricePaid(price);
-                    break;
+                    default:
+                        o.setPricePaid(price);
+                        break;
+                }
             }
         }
     }
