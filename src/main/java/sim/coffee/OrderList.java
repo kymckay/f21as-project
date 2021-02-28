64e4da5bdefbbefbd6d7135f8a2b2b9dfce63708
linarietuma <linarietuma@192.168.31.119>
2021-02-28 16:10:19 +0200

    Remove writeReport()

--- src/main/java/sim/coffee/OrderList.java
+++ src/main/java/sim/coffee/OrderList.java
@@ -65,19 +65,6 @@ public class OrderList {
 		}
 	}
 
-	public void writeFile(String variable) {
-		FileWriter writeFile;
-
-		try {
-			writeFile = new FileWriter("output.txt");
-			writeFile.write(variable);
-			writeFile.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	// Simple wrapper method
 	public boolean add(Order o) {
 		return orders.add(o);
