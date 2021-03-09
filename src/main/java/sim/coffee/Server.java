b571969b87019925c355a80093682afa379c36b3
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-09 13:24:34 +0000

    Create Server.java
    Started implementing

---
+++ src/main/java/sim/coffee/Server.java
@@ -0,0 +1,19 @@
+package sim.coffee;
+
+public class Server implements Runnable{
+    private Queue queue;
+
+    public Server(Queue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (!queue.getDone()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
+        }
+        int number = queue.getOrder();
+    }
+    
+}
