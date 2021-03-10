b7e6513bff97ce7de4e7c9b20e818382886be47a
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-10 16:34:38 +0000

    Update Manager to fit new task of the program

--- src/main/java/sim/coffee/Manager.java
+++ src/main/java/sim/coffee/Manager.java
@@ -1,34 +1,32 @@
 package sim.coffee;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Manager {
     public static void main(String[] args) {
-        Menu menu;
-        OrderList orders;
+        // Queue of orders populated by producer for staff to serve
+        Queue orders = new Queue();
+        Thread producer;
 
         // If input menu or orders file isn't found program cannot continue
         try {
-            menu = new Menu("data/menu.csv");
-
-            // List of placed orders uses a linked list since it's most often being appended
-            // Could become quite long if a large quantity of orders are processed
-            orders = new OrderList("data/orders.csv", new LinkedList<>());
+            // Producer inserts input file of orders into shared queue for staff
+            new Thread(new Producer(
+                "data/orders.csv",
+                new Menu("data/menu.csv"),
+                orders
+            ));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
             return; // This just placates the linter
         }
 
-        // Basket uses an array list since it's more often being accessed by the table display
-        // Additonally basket is likely never going to contain a significantly long list
-        OrderBasket basket = new OrderBasket(menu, orders, new ArrayList<>());
+        Thread server1 = new Thread(new Server(orders));
+        Thread server2 = new Thread(new Server(orders));
 
-        new CustomerGUI(
-            new MenuTableModel(menu),
-            new BasketTableModel(basket)
-        );
+        server1.start();
+        server2.start();
+        producer.start();
     }
 }
