d741e15e87faa41b16a6fc8c45da3cddcaf88035
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-10 17:39:32 +0000

    Resolve compilation complaints
    Just adding in the structure to compile this, will be replaced by
    functional implementetion when all merged together

--- src/main/java/sim/coffee/Manager.java
+++ src/main/java/sim/coffee/Manager.java
@@ -1,25 +1,26 @@
 package sim.coffee;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Manager {
     public static void main(String[] args) {
         // Queue of orders populated by producer for staff to serve
         Queue orders = new Queue();
-        Thread producer;
 
         // If input menu or orders file isn't found program cannot continue
         try {
             // Producer inserts input file of orders into shared queue for staff
-            new Thread(new Producer(
-                "data/orders.csv",
+            Thread producer = new Thread(new Producer(
+                new File("data/orders.csv"),
                 new Menu("data/menu.csv"),
                 orders
             ));
+
+            producer.start();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
-            return; // This just placates the linter
         }
 
         Thread server1 = new Thread(new Server(orders));
@@ -27,6 +28,5 @@ public class Manager {
 
         server1.start();
         server2.start();
-        producer.start();
     }
 }
