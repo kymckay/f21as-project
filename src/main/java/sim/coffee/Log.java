3ac5dbfc37ac037eed85b2f0bfd2f50537a29b1b
LinaRietuma <lina.rietuma@gmail.com>
2021-03-10 18:28:14 +0200

    Implement basic functionality

--- src/main/java/sim/coffee/Log.java
+++ src/main/java/sim/coffee/Log.java
@@ -2,5 +2,19 @@ package sim.coffee;
 
 public class Log {
 	private static Log instance;
+	private StringBuilder log;
 	
+	private Log() {
+		log = new StringBuilder();
+	}
+	
+	public void addOrder(StringBuilder order) {
+		log.append(order + "\n");
+	}
+	
+	public static Log getInstance() {
+		if (instance == null)
+			instance = new Log();
+		return instance;
+	}
 }
