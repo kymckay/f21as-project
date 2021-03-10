3cc2b22bf027971ac214f3abdd3e8aeda8f7f7fc
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-10 17:51:33 +0000

    Add initial producer implementation
    Just reads through the file adding groupings of customer orders to the
    queue at a time

---
+++ src/main/java/sim/coffee/Producer.java
@@ -0,0 +1,97 @@
+package sim.coffee;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+
+/**
+ * This class simulates the outside world, producing customers as they arrive
+ * and join the queue for staff to serve (in the form of Order groupings)
+ *
+ * Orders are read in from a supplied CSV file where each line is an item in a customer's order.
+ * Order groupings with the same customer are all one "order" and added to the queue together.
+ */
+public class Producer implements Runnable {
+    File in; // orders come from here
+    Menu menu; // used to produce orders
+    Queue out; // orders go here
+
+    String frontOfLine; // Customer ID currently at the front of the line
+    LinkedList<Order> basket = new LinkedList<>(); // Contains orders for customer at front of line
+
+    public Producer(File in, Menu menu, Queue out) {
+        this.in = in;
+        this.menu = menu;
+        this.out = out;
+    }
+
+    /**
+     * Opens the orders file and starts populating the queue
+     */
+    @Override
+    public void run() {
+        // Sleep some time based on the number of items to order (simulate customer deciding)
+        // Put orders for one customer into the queue at once (simulates "checkout")
+        // Sleep randomly between each customer (simulate time between arrival)
+
+        // Buffered reader provides an efficient way to read files line by line
+        int lineNum = 1;
+        try (
+            BufferedReader input = new BufferedReader(new FileReader(in));
+        ) {
+            String line;
+            // Returns null when EOF reached
+            while ((line = input.readLine()) != null) {
+                // A single customer's items go into their basket until they checkout
+                // before the next customer
+                Order item = processLine(line);
+
+                // Next customer reached, checkout basket and empty
+                if (item.getCustomerID().equals(frontOfLine)) {
+                    out.addOrder(basket.toArray()); // TODO: This will add en empty array first iteration
+                    basket.clear();
+                }
+
+                basket.add(item);
+
+                lineNum++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(in.getName() + " does not exist.");
+        } catch (IOException e) {
+            System.out.println(in.getName() + " could not be read.");
+        } catch(IllegalArgumentException e) {
+            System.out.println("Order parsing error on line " + lineNum + ": " + e.getMessage());
+        }
+    }
+
+    private Order processLine(String line) {
+        // Remove whitespace while splitting using regex delimiter
+        // Java's split operator discards empty strings by default, -1 keeps them (empty
+        // csv columns are valid)
+        String[] cols = line.split("\\s*,\\s*", -1);
+
+        // All rows in csv file have same columns
+        if (cols.length == 6) {
+            LocalDateTime timestamp = LocalDateTime.parse(cols[0], DateTimeFormatter.ISO_DATE_TIME);
+            String custId = cols[1];
+            String itemId = cols[2];
+            BigDecimal priceFull = new BigDecimal(cols[3]);
+            BigDecimal pricePaid = new BigDecimal(cols[4]);
+            String itemDetails = cols[5];
+
+            // OrderItem subclasses store the item permutations ordered
+            OrderItem newItem = new OrderItem(itemId, itemDetails, priceFull, pricePaid);
+
+            return new Order(timestamp, custId, newItem);
+        } else {
+            throw new IllegalArgumentException("Line contains wrong number of values");
+        }
+    }
+}
--- src/main/java/sim/coffee/Queue.java
+++ src/main/java/sim/coffee/Queue.java
@@ -5,18 +5,18 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class Queue {
-	
+
 	private LinkedList<Order> queue;
 	private boolean empty;
 	private boolean done;
-	
+
 	public Queue() {
 		queue = new LinkedList<Order>();
 		empty = true;
 		done = false;
 	}
-	
-	// return customer id at the top of the queue + details for all orders with the same customer id 
+
+	// return customer id at the top of the queue + details for all orders with the same customer id
 	// assume that the same customer is given a different customer IDs for different purchases
 	// a purchase can include multiple items (e.g. orders)
 	public synchronized StringBuilder getCustomerOrder() {
@@ -26,15 +26,15 @@ public class Queue {
 			}
 		 	catch (InterruptedException e) {
 		 		e.printStackTrace();
-		 	}	
+		 	}
 		}
-		
+
 		StringBuilder customerOrder = new StringBuilder();
-		// depending on what we include in the order.csv, this could also be the name of the customer  
+		// depending on what we include in the order.csv, this could also be the name of the customer
 		customerOrder.append(queue.getFirst().getCustomerID() + "\n");
-		 
+
 		String custID = queue.getFirst().getCustomerID();
-		LinkedList<Order> toRemove = new LinkedList<Order>(); 
+		LinkedList<Order> toRemove = new LinkedList<Order>();
 		// adds all orders with the same customer id to string
 		for(Order order : queue) {
 			if(order.getCustomerID().equals(custID)) {
@@ -42,17 +42,17 @@ public class Queue {
 				toRemove.add(order);
 			}
 		}
-		// removes all orders with the customer id matching the one on top of the queue 
+		// removes all orders with the customer id matching the one on top of the queue
 		queue.removeAll(toRemove);
 		toRemove.clear();
-		
+
 		if (queue.size() == 0) {
 			empty = true;
 			notifyAll();
 		}
 		return customerOrder;
 	}
-	
+
 	// adds orders to the queue
 	// assume simultaneous addition of multiple orders with the same customer id is handled in producer class
 	public synchronized void addOrder(Order o) {
@@ -60,15 +60,15 @@ public class Queue {
 		empty = false;
 		notifyAll();
 	}
-	
+
 	public boolean getDone() {
 		return done;
 	}
-	
+
 	public void setDone() {
 		done = true;
 	}
- 	
+
 	// determine the number of customers in the queue (i.e. unique IDs)
 	protected int getCustomerCount() {
 		int customerCount = 0;
@@ -77,17 +77,17 @@ public class Queue {
  			if (!(o.getCustomerID().equals(uniqueId))) {
  				uniqueId = o.getCustomerID();
  				customerCount++;
- 			}	
+ 			}
  		}
  		return customerCount;
 	}
-	
+
 	// displays queue for the GUI
 	public StringBuilder displayQueue() {
 		StringBuilder queueLog = new StringBuilder();
  		queueLog.append("Customers in the queue: ");
  		queueLog.append(getCustomerCount() + "\n");
- 		
+
  		// adds customer Id + nr of items purchased as value pairs in a HashMap
  		// this seems needlessly complex?
  		Map<String, Integer> purchase = new HashMap<String, Integer>();
@@ -95,12 +95,12 @@ public class Queue {
  			Integer nrOfOrders = purchase.get(o.getCustomerID());
  			if (nrOfOrders == null) { // if the nr of items is 0, customer Id + nr of items = 1  is added to the map
  				purchase.put(o.getCustomerID(), 1);
- 			} 
- 			else { // otherwise the nr of items is incremented 
+ 			}
+ 			else { // otherwise the nr of items is incremented
  				purchase.put(o.getCustomerID(), nrOfOrders + 1);
  			}
  		}
- 		
+
  		// add all entries in the HashMap to queueLog
  		for (Map.Entry<String, Integer> pair : purchase.entrySet()) {
  			queueLog.append(String.format("%-10s", pair.getKey()));
@@ -109,4 +109,7 @@ public class Queue {
  		}
  		return queueLog;
  	}
+
+    public void addOrder(Object[] array) {
+    }
 }
