04479b58fd5d51fed3208bdbeb42a6a3ebb84ad2
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-14 20:50:27 +0000

    Revert "Update KitchenStaff.java"
    This reverts commit e4c09249cbe9b9d7580f76b374de5baa0673cc11.

--- src/main/java/sim/coffee/KitchenStaff.java
+++ src/main/java/sim/coffee/KitchenStaff.java
@@ -7,19 +7,5 @@ public class KitchenStaff implements Runnable{
         this.queue = queue;
     }
 
-    /**
-     * Run only while its not done and queue isn't empty.
-     * Should be the same as server just that it takes longer to prepare
-     * food as compare to serve food.
-     */
-    @Override
-    public void run() {
-        int queueSize = queue.getCustomerOrder().length;
-        while (!queue.getDone() && queueSize > 0) {
-            try {
-                // Tries to simulate no. of orders * time it take to prepare one order
-                Thread.sleep(60000*queueSize);
-            } catch (InterruptedException e) {}
-        }
-    }
+
 }
