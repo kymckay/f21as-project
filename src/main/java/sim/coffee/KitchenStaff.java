f9a8e9b89e76e3cba140529392b9c2f03f337f18
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-14 20:56:17 +0000

    Create KitchenStaff.java

---
+++ src/main/java/sim/coffee/KitchenStaff.java
@@ -0,0 +1,25 @@
+package sim.coffee;
+
+public class KitchenStaff implements Runnable{
+    private SharedQueue queue;
+
+    public KitchenStaff(SharedQueue queue) {
+        this.queue = queue;
+    }
+
+    /**
+     * Run only while its not done and queue isn't empty.
+     * Should be the same as server just that it takes longer to prepare
+     * food as compare to serve food.
+     */
+    @Override
+    public void run() {
+        while (!queue.getDone() && queue.getSharedQueueSize() > 0) {
+            try {
+                // Tries to simulate no. of orders * time it take to prepare one order
+                int orderSize = queue.getCustomerOrder().length;
+                Thread.sleep(60000*orderSize);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
