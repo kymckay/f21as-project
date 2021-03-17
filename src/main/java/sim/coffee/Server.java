b3b078a7b629354d8ece918e5238a8dd9d35814e
LinaRietuma <lina.rietuma@gmail.com>
2021-03-17 20:28:27 +0200

    Implement displayCurrentOrder() and Observer pattern

--- src/main/java/sim/coffee/Server.java
+++ src/main/java/sim/coffee/Server.java
@@ -1,10 +1,17 @@
 package sim.coffee;
 
-public class Server implements Runnable{
+import java.util.LinkedList;
+
+public class Server implements Runnable, Subject {
     private SharedQueue queue;
+    private StringBuilder currentOrder;
+    private LinkedList<Observer> observers;
 
     public Server(SharedQueue queue) {
         this.queue = queue;
+        currentOrder = new StringBuilder();
+        observers = new LinkedList<Observer>();
+        
     }
 
     /**
@@ -12,13 +19,45 @@ public class Server implements Runnable{
      */
     @Override
     public void run() {
-        while (!queue.getDone() && !queue.isEmpty()) {
+        while (!queue.getDone()) {
+        	Order [] order = queue.getCustomerOrder();
+        	setCurrentOrder(order);
             try {
                 // Tries to simulate no. of orders * time it take to prepare one order
-                // queue.getCustomerOrder() // TODO: Implement in the future
-                int orderSize = queue.getCustomerOrder().length;
-                Thread.sleep(2000*orderSize);
+                int orderSize = order.length;
+                Thread.sleep(10000 * orderSize);
             } catch (InterruptedException e) {}
         }
     }
+    
+    public void setCurrentOrder(Order[] o) {
+    	currentOrder.replace(0, currentOrder.length(), "");
+    	currentOrder.append("Customer being served: ");
+    	currentOrder.append(o[0].getCustomerID() + "\n");
+    	currentOrder.append("Customer being served: ");
+    	currentOrder.append("Orders: \n");
+    	for (Order order : o) {
+    		currentOrder.append(String.format("%10s", order.getItemId()) + "\n");
+    	}
+    }
+    
+    public StringBuilder getCurrentOrder() {
+    	return currentOrder;
+    }
+
+    // implements Observer pattern
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	public void removeObserver(Observer o) {
+		observers.remove(o);	
+	}
+
+	public void notifyObservers() {
+		for (Observer o : observers) {
+			o.update();
+		}
+		
+	}
 }
