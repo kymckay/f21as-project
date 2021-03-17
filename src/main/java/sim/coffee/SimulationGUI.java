4bb82c0276352a98cf1f06dab28012e29d53dcee
LinaRietuma <lina.rietuma@gmail.com>
2021-03-17 21:44:37 +0200

    Implement layout and basic functionality

--- src/main/java/sim/coffee/SimulationGUI.java
+++ src/main/java/sim/coffee/SimulationGUI.java
@@ -21,10 +21,12 @@ public class SimulationGUI extends JFrame implements Observer{
 	private JTextArea queue1, queue2;
 	private JPanel serverStaff;
 	private SharedQueue queue;
+	private int nrOfThreads;
 	
-	public SimulationGUI(SharedQueue queue) {
+	public SimulationGUI(SharedQueue queue,  int nrOfThreads) {
 		
 		this.queue = queue;
+		this.nrOfThreads = nrOfThreads;
 		queue.registerObserver(this);
 		
 		setTitle("Coffee Shop Simulation");
@@ -36,86 +38,67 @@ public class SimulationGUI extends JFrame implements Observer{
 		setup();
 		pack();
 		setVisible(true);
-		update();
 	}
 	
+	// sets the overall GUI layout
 	private void setup() {
 		JPanel main = new JPanel(new GridLayout(4,1));
+		
 		main.add(setupQueue1());
-		main.add(setupServer(3));
+		main.add(setupServer());
 		main.add(setupQueue2());
-		//main.add(setupKitchenStaff());
+		main.add(setupKitchen());
 		
 		main.setBorder(new EmptyBorder(10, 10, 10, 10));
 		add(main, BorderLayout.CENTER);
 	}
 	
+	// sets up the queue with customers waiting to be served 
 	private JScrollPane setupQueue1() {
 		
 		queue1 = new JTextArea();
 		queue1.setEditable(false);
 		JScrollPane queue1Pane = new JScrollPane(queue1);
-	
 		return queue1Pane;
 	}
 	
-	// the number of JPanels depends on the number of threads
-	private JPanel setupServer(int i) {
-		serverStaff = new JPanel(new GridLayout(1,i));
+	// sets up the server section
+	// TODO: will need to adjust GUI width if more than 3 servers are added
+	// probably should set a limit to the nr of threads that can be initiated
+	private JPanel setupServer() {
 		
-		for (int x = 1; x <= i; x++) {
-			serverStaff.add(server(x));
+		serverStaff = new JPanel(new GridLayout(1, nrOfThreads));
+		// the number of servers depends on the number of threads specified upon instantiating the GUI
+		// in every iteration Server object is created which is then used to start a thread and create a ServerGUI
+		// ServerGUI is then added to the main serveStaff JPanel 
+		for (int i = 1; i <= nrOfThreads; i++) {
+			Server staff = new Server(queue);
+			Thread thread = new Thread(staff);
+			serverStaff.add(new ServerGUI(staff, i));
+			thread.start();
 		}
-		
 		return serverStaff;
 	}
 	
-	private JTextArea server(int i) {
-		
-		JTextArea server = new JTextArea();
-		server.setEditable(false);
-		String borderTitle = "Server " + i;
-		Border border1 = BorderFactory.createTitledBorder(borderTitle);
-		
-		server.setBorder(border1);
-		return server;
+	// TODO: will have to set this up once KitchenStaff class is implemented
+	private JTextArea setupKitchen() {
+		JTextArea kitchenStaff = new JTextArea();
+		kitchenStaff.setText("This will show kitchen staff, probably similar to how serveing staff's displayed?");
+		return kitchenStaff;
 	}
 	
+	// TODO: will set this up when the shared queue between servers and kitchen staff is implemented
 	private JScrollPane setupQueue2() {
-		
 		queue2 = new JTextArea();
 		queue2.setEditable(false);
 		JScrollPane queue2Pane = new JScrollPane(queue2);
-
+		queue2.setText("This will have the kitchen queue");
 		return queue2Pane;
 	}
-
-
 	
-//	private JPanel setupKitchenStaff() {
-//		server = new JPanel(new GridLayout(1,2));
-//		
-//		JTextArea server1 = new JTextArea();
-//		server1.setEditable(false);
-//		Border border1 = BorderFactory.createTitledBorder(" Server 1 ");
-//		
-//		server1.setBorder(border1);
-//		
-//		JTextArea server2 = new JTextArea();
-//		server2.setEditable(false);
-//		Border border2 = BorderFactory.createTitledBorder(" Server 2 ");
-//		server2.setBorder(border2);
-//		
-//		server.setBackground(Color.WHITE);
-//		server.add(server1);
-//		server.add(server2);
-//		return server;
-//	}
-
+	// Observer method
 	public void update() {
 		StringBuilder customerQueue = queue.getQueue();
-		queue1.setText(customerQueue.toString());
-		
+		queue1.setText(customerQueue.toString());	
 	}
-	
 }
