205c755d64d2403f1030f550736c2338d5214dbf
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-20 12:02:18 +0000

    Update PriorityQueue.java

--- src/main/java/sim/coffee/PriorityQueue.java
+++ src/main/java/sim/coffee/PriorityQueue.java
@@ -1,5 +1,11 @@
 package sim.coffee;
 
+import java.util.LinkedList;
+
 public class PriorityQueue {
+
+    private LinkedList<Order[]> queue = new LinkedList<>();
+    private boolean empty = true;
+    private boolean done = true;
     
 }
