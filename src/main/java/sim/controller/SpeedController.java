004ee8a46ac4e587e6c4b2806ea084a197757191
LinaRietuma <lina.rietuma@gmail.com>
2021-03-26 20:14:50 +0200

    Fix speed update logic

--- src/main/java/sim/controller/SpeedController.java
+++ src/main/java/sim/controller/SpeedController.java
@@ -20,31 +20,30 @@ public class SpeedController {
 		this.server = server;
 		this.gui = gui;
 		baseSpeed = server.getBaseSpeed();
-		
+		// register SliderListener with the gui
 		gui.addSetListener(new SliderListener());
 	}
 	
+	// inner SliderListener class responds when user selects a new simulation speed
 	public class SliderListener implements ChangeListener {
-
+		
 		@Override
 		public void stateChanged(ChangeEvent e) {
 			JSlider source = (JSlider)e.getSource();
 			if (!source.getValueIsAdjusting()) { 
-				int speed = (int)source.getValue();
-				if (speed < 0) {
-					newSpeed = baseSpeed / (speed * -1);
+				int speed = (int)source.getValue(); // gets speed selected on the slider
+				if (speed < 0) { // speed has been decreased
+					newSpeed = baseSpeed * (speed * -1);
 					server.setSpeed(newSpeed);
 				} 
-				else if (speed > 0) {
-					newSpeed = baseSpeed * (speed);
+				else if (speed > 0) { // speed has been increased
+					newSpeed = baseSpeed / (speed);
 					server.setSpeed(newSpeed);
 				}
-				else {
-					server.setSpeed(baseSpeed);	
+				else { // speed selected to normal 
+					server.setSpeed(baseSpeed);	 
 				}
 			}
 		}
-		
 	}
-
 }
