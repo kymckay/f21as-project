410f0b2365b4bbf97cd5b9f4ebac4087be35c7b0
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2021-03-27 22:15:34 +0000

    Rename main code file for clarity

--- src/main/java/sim/app/Simulation.java
+++ src/main/java/sim/app/Simulation.java
@@ -3,7 +3,7 @@ package sim.app;
 import sim.model.CoffeeShop;
 import sim.views.SimulationGUI;
 
-public class Manager {
+public class Simulation {
     public static void main(String[] args) {
         new SimulationGUI(new CoffeeShop(3));
     }
