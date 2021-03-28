46ef8fae406361b3e6b84d07e1f549f57012540a
Jonathan Lee <65177718+jonleesy@users.noreply.github.com>
2021-03-28 17:52:49 +0100

    Update CoffeeShop.java

--- src/main/java/sim/model/CoffeeShop.java
+++ src/main/java/sim/model/CoffeeShop.java
@@ -25,7 +25,7 @@ public class CoffeeShop implements Subject {
     public CoffeeShop(int numStaff) {
         // Producer inserts input file of orders into shared queue for staff
         Thread producer = new Thread(new Producer(new File("data/orders.csv"), customers));
-        Thread priorityProducer = new Thread(new Producer(new File("data/orders.csv"), customers));
+        Thread priorityProducer = new Thread(new Producer(new File("data/orders.csv"), priorityOrders));
 
         // Staff members consumes the queue of customer orders
         for (int i = 0; i < numStaff; i++) {
